8047585c490869e04dc5085a5c16274b0427c5e7
andersonsaj <andersonsb09@hotmail.com>
2020-03-23 16:45:22 -0300

    Criando operação de instanciação

--- src/main/java/com/teste/vandamc/VendamcApplication.java
+++ src/main/java/com/teste/vandamc/VendamcApplication.java
@@ -1,12 +1,30 @@
 package com.teste.vandamc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class VendamcApplication {
+import com.teste.vandamc.domain.Categoria;
+import com.teste.vandamc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class VendamcApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendamcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 }
