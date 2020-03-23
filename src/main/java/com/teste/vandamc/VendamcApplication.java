9f79a76faf1765f4dd7db6b7e3f29e8b01879423
andersonsaj <andersonsb09@hotmail.com>
2020-03-23 11:33:45 -0300

    Projeto criado

---
+++ src/main/java/com/teste/vandamc/VendamcApplication.java
@@ -0,0 +1,12 @@
+package com.teste.vandamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendamcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendamcApplication.class, args);
+	}
+}
