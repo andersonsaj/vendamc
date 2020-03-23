c03c937b1a8a5d4c8a2341e34aaac58373013bcb
andersonsaj <andersonsb09@hotmail.com>
2020-03-23 13:40:05 -0300

    Classe Categoria criada

---
+++ src/main/java/com/teste/vandamc/resources/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.teste.vandamc.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.vandamc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> list = Arrays.asList(cat1,cat2);
+		return list;
+	}
+}
