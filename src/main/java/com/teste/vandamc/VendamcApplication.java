2439405439eecb5a11f2c149ee32636b45516968
andersonsaj <andersonsb09@hotmail.com>
2020-03-29 22:16:53 -0300

    Estado e Cidade

--- src/main/java/com/teste/vandamc/VendamcApplication.java
+++ src/main/java/com/teste/vandamc/VendamcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.teste.vandamc.domain.Categoria;
+import com.teste.vandamc.domain.Cidade;
+import com.teste.vandamc.domain.Estado;
 import com.teste.vandamc.domain.Produto;
 import com.teste.vandamc.repositories.CategoriaRepository;
+import com.teste.vandamc.repositories.CidadeRepository;
+import com.teste.vandamc.repositories.EstadoRepository;
 import com.teste.vandamc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,11 @@ public class VendamcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository repoProduto;
+	
+	@Autowired
+	private CidadeRepository repoCidade;
+	@Autowired
+	private EstadoRepository repoEstado;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendamcApplication.class, args);
@@ -40,8 +49,22 @@ public class VendamcApplication implements CommandLineRunner{
 		p2.getCategoria().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategoria().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "MinasGerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1= new Cidade(null, "Uberlândia", est1);
+		Cidade c2= new Cidade(null, "São Paulo", est2);
+		Cidade c3= new Cidade(null, "Campinas", est2);
+		
 		repoCategoria.saveAll(Arrays.asList(cat1, cat2));
 		repoProduto.saveAll(Arrays.asList(p1,p2,p3));
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		repoEstado.saveAll(Arrays.asList(est1, est2));
+		repoCidade.saveAll(Arrays.asList(c1, c2, c3));
+		
 		
 	}
 }
--- src/main/java/com/teste/vandamc/domain/Estado.java
+++ src/main/java/com/teste/vandamc/domain/Estado.java
@@ -1,44 +1,33 @@
 package com.teste.vandamc.domain;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-
+import javax.persistence.OneToMany;
 @Entity
-public class Produto implements Serializable{
+public class Estado implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	private Double preco;
-	
-	@JsonIgnore
-	@ManyToMany
-	@JoinTable(name = "produto_categoria", 
-	joinColumns = @JoinColumn(name = "produto_id"), 
-	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
-	private Set<Categoria> categorias = new HashSet<>();
-	
-	public Produto() {
+	@OneToMany
+	@JoinColumn(name="estado")
+	private List<Cidade> cidades = new ArrayList<>();
+
+	public Estado() {
 	}
 
-	public Produto(Integer id, String nome, Double preco) {
+	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
-		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -57,20 +46,12 @@ public class Produto implements Serializable{
 		this.nome = nome;
 	}
 
-	public Double getPreco() {
-		return preco;
+	public List<Cidade> getCidades() {
+		return cidades;
 	}
 
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
-
-	public Set<Categoria> getCategoria() {
-		return categorias;
-	}
-
-	public void setCategoria(Set<Categoria> categoria) {
-		this.categorias = categoria;
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
 	}
 
 	@Override
@@ -89,7 +70,7 @@ public class Produto implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Produto other = (Produto) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -97,4 +78,6 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
+	
+	
 }
